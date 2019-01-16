303268365db4f8b6858ad8b8f723c749bc199146
Vadim Chervyakov <Vadim.Chervyakov@kaspersky.com>
2019-01-16 18:15:53 +0700

    nm

--- spring/cardatabase/src/main/java/com/packt/cardatabase/CardatabaseApplication.java
+++ spring/cardatabase/src/main/java/com/packt/cardatabase/CardatabaseApplication.java
@@ -2,18 +2,35 @@ package com.packt.cardatabase;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.packt.cardatabase.domain.Car;
+import com.packt.cardatabase.domain.CarRepository;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CardatabaseApplication {
 
 	private final static Logger logger = LoggerFactory.getLogger(CardatabaseApplication.class);
 
+	@Autowired
+	private CarRepository repository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CardatabaseApplication.class, args);
 		logger.info("Hello world!");
 	}
 
+	@Bean
+	CommandLineRunner runner() {
+		return args -> {
+			repository.save(new Car("Ford", "Mustang", "Red", "ADF-1211", 2017, 59000));
+			repository.save(new Car("Nissan", "Leaf", "White", "SSJ-3002", 2014, 29000));
+			repository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2018, 39000));
+		};
+	}
 }
 
--- spring/cardatabase/src/main/java/com/packt/cardatabase/domain/CarRepository.java
+++ spring/cardatabase/src/main/java/com/packt/cardatabase/domain/CarRepository.java
@@ -2,6 +2,15 @@ package com.packt.cardatabase.domain;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface CarRepository extends CrudRepository<Car, Long> {
 
+    List<Car> findByBrand(String brand);
+
+    List<Car> findByColor(String color);
+
+    List<Car> findByYear(int year);
+
+    List<Car> findByBrandAndModel(String brand, String model);
 }
